5e4c36a5f8d6489fe7b9cb5c8e4c5438002fad18
Nacho228 <6319@holbertonstudents.com>
2023-08-04 11:25:32 -0700

    que esta mal ?

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -55,7 +55,7 @@ class FileStorage:
             pass
 
     def delete(self, obj=None):
-        """ Delete instance method"""
+        """ Delete instance method """
         if obj is None:
             return
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
