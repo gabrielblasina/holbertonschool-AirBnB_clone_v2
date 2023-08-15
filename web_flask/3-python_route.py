3cf44087c01d639f1f29aba8df72a774adc06353
Nacho228 <6319@holbertonstudents.com>
2023-08-15 11:09:18 -0700

    3-python_route.py

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" Web flask """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_with_text(text):
+    ftxt = text.replace('_', ' ')
+    return f"C {ftxt}"
+
+
+@app.route('/Python/<text>', strict_slashes=False)
+def python_with_text(text="is cool"):
+    formatted_txt = text.replace('_', ' ')
+    return f"Python {formatted_txt}"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
