e68af12f86a46466417a32b8254a3c5d929bd877
gabrielblasina <5722@holbertonstudents.com>
2023-08-18 15:30:02 -0700

    task-7 correction

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,6 +5,7 @@ The application listens on 0.0.0.0, port 5000.
 Routes:
     /states_list: HTML page with a list of all State objects in DBStorage.
 """
+
 from models import storage
 from flask import Flask
 from flask import render_template
@@ -14,10 +15,7 @@ app = Flask(__name__)
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-
-    States are sorted by name.
-    """
+    """Displays an HTML page with a list of all State"""
     states = storage.all("State")
     return render_template("7-states_list.html", states=states)
 
